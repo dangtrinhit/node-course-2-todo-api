270ec0d8b0e07815ecdae74a664644b1a286d37d
dangtrinhit <dangtrinh@Phams-MacBook-Pro.local>
2018-01-12 14:18:08 +0700

    Test PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id:new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  complete: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -99,7 +101,7 @@ describe('GET /todos/:id' ,() => {
   })
 })
 
-describe ('DELETE /todo/:id', () => {
+describe ('DELETE /todos/:id', () => {
   it('should remove a todo', (done) => {
     var hexId = todos[1]._id.toHexString()
     request(app)
@@ -121,6 +123,7 @@ describe ('DELETE /todo/:id', () => {
 
   it('should return 404 if todo not found', (done)=> {
     var hexId = new ObjectID().toHexString()
+
     request(app)
       .get(`/todos/${hexId}`)
       .expect(404)
@@ -131,6 +134,44 @@ describe ('DELETE /todo/:id', () => {
     request(app)
       .get(`/todos/123abc`)
       .expect(404)
+      .end(done)
+  })
+})
+
+describe ('PATCH /todos/:id', ()=>{
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString()
+    var text = 'This should be the new text'
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed:true,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(true)
+        expect(typeof res.body.todo.completedAt).toBe('number')
+      })
+      .end(done)
+  })
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString()
+    var text = 'This should be the new text!!'
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed:false,
+        text
+      })
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(false)
+        expect(res.body.todo.completedAt).toBeFalsy()
+      })
       .end(done)
   })
 })
